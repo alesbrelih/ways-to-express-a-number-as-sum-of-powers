8ead3b4e8f3c8e84d27882ca1ca8bf623e09c053
ales.brelih@gmail.com <ales.brelih@gmail.com>
2018-01-11 13:55:11 +0100

    Algorithm implementation

---
+++ NumberAsSumOfNumersOnPowerOf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumberAsSumOfNumersOnPowerOf
+{
+    class Program
+    {
+            
+        public static int CountWaysRecursive(int number, int power, int currentNum = 1)
+        {
+            int val = number - (int)Math.Pow(currentNum, power);
+
+            if (val < 0)
+            {
+                return 0;
+            }
+            if (val == 0)
+            {
+                return 1;
+            }
+
+            return CountWaysRecursive(val, power, currentNum + 1) + CountWaysRecursive(number, power, currentNum + 1);
+        }
+        public static int CountWays(int number, int power)
+        {
+            return CountWaysRecursive(number, power);
+        }
+
+        static void Main(string[] args)
+        {
+            int result = CountWays(500, 2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
